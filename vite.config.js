67bb8a273a75c924b8c1bef17f0d5ddb6cf76b2d
SK MD Shadman Sakib <161970909+skmdshadmansakib@users.noreply.github.com>
2025-09-26 00:03:16 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // <-- Add this for GitHub Pages
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 })
